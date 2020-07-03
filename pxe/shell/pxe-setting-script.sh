ad311a6f56e4dd91c45e8aa1ba9107c00160add5
Stars_Kim <Stars_Kim@163.com>
2020-07-03 22:08:20 +0800

    update pxe

--- pxe/shell/pxe-setting-script.sh
+++ pxe/shell/pxe-setting-script.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-######## hostname ########
-LOCAL_IPADDR=$(hostname -I | cut -d ' ' -f 1)
-PTR_ANSWER=$(dig +short -x "$LOCAL_IPADDR")
-ENS=$(cat /proc/net/dev | awk '{i++; if(i>3){print $1}}' | sed 's/^[\t]*//g' | sed 's/[:]*$//g')
-if [ -z "$PTR_ANSWER" ] ; then
-    hostname="linux_$(sed 's/://g' < /sys/class/net/$ENS/address | cut -c 7-12)"
-else
-    hostname=$(echo "$PTR_ANSWER" | cut -d '.' -f 1)
-fi
-
-hostnamectl set-hostname "$hostname"
-
 ######## sysctl ########
 cat <<EOF >> /etc/sysctl.conf
 
