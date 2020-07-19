03c3dc31387ac5cb84a71de51f1290a85c71c544
Stars_Kim <Stars_Kim@163.com>
2020-07-20 04:07:41 +0800

    update pxe

--- pxe/shell/installgit.sh
+++ pxe/shell/installgit.sh
@@ -9,5 +9,7 @@ make configure
 ./configure --prefix=/usr/local/git
 make profix=/usr/local/git
 make install
-echo  "export GIT_HOME=/usr/local/git" >> /etc/profile
-echo  "export PATH=$PATH:$GIT_HOME/bin" >> /etc/profile
+[ -z "`grep ^'export GIT_HOME=' /etc/profile`" ] && { [ -z "`grep ^'export PATH=' /etc/profile`" ] && echo  "export GIT_HOME=/usr/local/git" >> /etc/profile || sed -i "s@^export PATH=@export GIT_HOME=/usr/local/git\nexport PATH=@" /etc/profile; } || sed -i "s@^export GIT_HOME=.*@export GIT_HOME=/usr/local/git@" /etc/profile
+[ -n "`grep ^'export PATH=' /etc/profile`" -a -z "`grep '$GIT_HOME/bin' /etc/profile`" ] && sed -i "s@^export PATH=\(.*\)@export PATH=\$GIT_HOME/bin:\1@" /etc/profile
+[ -z "`grep ^'export PATH=' /etc/profile | grep '$GIT_HOME/bin'`" ] && echo 'export PATH=$GIT_HOME/bin:$PATH' >> /etc/profile
+. /etc/profile
--- pxe/shell/pxe-setting-script.sh
+++ pxe/shell/pxe-setting-script.sh
@@ -154,7 +154,7 @@ export https_proxy=http://10.0.0.251:7890 http_proxy=http://10.0.0.251:7890 all_
 sh -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/starskim/cdn/pxe/shell/ohmyzsh.sh)"
 
 ######## git ########
-# sh -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/starskim/cdn/pxe/shell/installgit.sh)"
+sh -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/starskim/cdn/pxe/shell/installgit.sh)"
 
 ######## 删除预设的脚本 ########
 sed -i '/.*pxe-setting-script\.sh.*/d' /etc/rc.d/rc.local
