21a8d71697b9bd4e4ff88a6386399720591afe2f
Stars_Kim <Stars_Kim@163.com>
2021-01-04 01:36:07 +0800

    Update

--- pxe/shell/pxe-setting-script.sh
+++ pxe/shell/pxe-setting-script.sh
@@ -132,15 +132,15 @@ echo "session required /usr/lib64/security/pam_limits.so" >> /etc/pam.d/login
 rm -f /etc/yum.repos.d/Centos*.repo
 rm -f /etc/yum.repos.d/epel*.repo
 yum clean all
-yum install -y iftop htop python-pip python3-pip python3-devel python3 bash-completion bash-completion-extras vim wget gcc \
-  ntpdate ntp ncurses-devel deltarpm gcc-c++ make cmake autoconf libjpeg libjpeg-devel libjpeg-turbo \
+yum install -y iftop htop python3-pip python3-devel python3 bash-completion vim wget gcc \
+  ncurses-devel gcc-c++ make cmake autoconf libjpeg libjpeg-devel libjpeg-turbo \
   libjpeg-turbo-devel libpng libpng-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel \
   krb5-devel libc-client libc-client-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel libaio numactl \
-  numactl-libs readline-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5-devel libidn libidn-devel openssl openssl-devel \
+  numactl-libs readline-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5-devel libidn openssl openssl-devel \
   net-tools libxslt-devel libicu-devel libevent-devel libtool libtool-ltdl bison gd-devel vim-enhanced pcre-devel libmcrypt \
-  libmcrypt-devel mhash mhash-devel mcrypt zip unzip sqlite-devel sysstat patch bc \
-  expect expat-devel oniguruma oniguruma-devel libtirpc-devel nss rsync rsyslog git lsof lrzsz \
-  psmisc which libatomic tmux gettext-devel perl-ExtUtils-MakeMaker redhat-lsb-core
+  libmcrypt-devel mhash mhash-devel zip unzip sqlite-devel sysstat patch bc \
+  expect expat-devel oniguruma libtirpc-devel nss rsync rsyslog git lsof lrzsz \
+  psmisc which libatomic tmux gettext-devel perl-ExtUtils-MakeMaker redhat-lsb-core docker-ce
 pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
 pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install glances
