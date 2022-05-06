a23372de7836e4fe0f326da803cb14ded17abe6d
Stars_Kim <stars.kim.ks@gmail.com>
2022-05-06 19:39:21 +0800

    Update pxe-setting-script.sh

--- pxe/shell/pxe-setting-script.sh
+++ pxe/shell/pxe-setting-script.sh
@@ -61,7 +61,7 @@ rm -f /etc/yum.repos.d/Centos*.repo
 rm -f /etc/yum.repos.d/epel*.repo
 rm -f /etc/yum.repos.d/zabbix*.repo
 yum clean all
-pkgList="iftop htop python2-pip python2-devel python2 python3-pip python3-devel python3 bash-completion bash-completion-extras vim wget gcc ntpdate ntp ncurses-devel deltarpm gcc-c++ make cmake autoconf libjpeg libjpeg-devel libjpeg-turbo libjpeg-turbo-devel libpng libpng-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel krb5-devel libc-client libc-client-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel libaio numactl numactl-libs readline-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5-devel libidn libidn-devel openssl openssl-devel net-tools libxslt-devel libicu-devel libevent-devel libtool libtool-ltdl bison gd-devel vim-enhanced pcre-devel libmcrypt libmcrypt-devel mhash mhash-devel mcrypt zip unzip sqlite-devel sysstat patch bc expect expat-devel oniguruma oniguruma-devel libtirpc-devel nss rsync rsyslog git lsof lrzsz psmisc which libatomic tmux gettext-devel perl-ExtUtils-MakeMaker redhat-lsb-core docker-ce cmake3 libzip libzip-devel wget chrony rpcgen zabbix-agent ffmpeg ffmpeg-devel"
+pkgList="iftop htop python2-pip python2-devel python2 python3-pip python3-devel python3 bash-completion bash-completion-extras vim wget gcc ntpdate ntp ncurses-devel deltarpm gcc-c++ make cmake autoconf libjpeg libjpeg-devel libjpeg-turbo libjpeg-turbo-devel libpng libpng-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel krb5-devel libc-client libc-client-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel libaio numactl numactl-libs readline-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5-devel libidn libidn-devel openssl openssl-devel net-tools libxslt-devel libicu-devel libevent-devel libtool libtool-ltdl bison gd-devel vim-enhanced pcre-devel libmcrypt libmcrypt-devel mhash mhash-devel mcrypt zip unzip sqlite-devel sysstat patch bc expect expat-devel oniguruma oniguruma-devel libtirpc-devel nss rsync rsyslog git lsof lrzsz psmisc which libatomic tmux gettext-devel perl-ExtUtils-MakeMaker redhat-lsb-core docker-ce docker-ce-cli containerd.io docker-compose-plugin cmake3 libzip libzip-devel wget chrony rpcgen zabbix-agent ffmpeg ffmpeg-devel"
 for Package in ${pkgList}; do
   yum -y install ${Package}
 done
@@ -85,10 +85,6 @@ fi
 
 ######## Docker ########
 if [ -e "$(which docker)" ]; then
-  if [ ! -e "/usr/local/bin/docker-compose" ];then
-    curl -L "https://github.starskim.com/https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-  fi  
   sudo mkdir /etc/docker
   cat <<EOF | sudo tee /etc/docker/daemon.json
   {
